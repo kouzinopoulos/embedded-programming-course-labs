b4dac23a9b513f1151a48315c6cd2f08e03cd600
Kouzinopoulos <P70092343@unimaas.nl>
2024-08-12 11:54:23 +0200

    Added Lab 1 exercises: STM32CubeMX, views and memory

---
+++ Lab 1/Core/Src/main.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+// Objectives of this lab:
+// 1. Familiarize with the STM32CubeIDE: Debug, run, breakpoints
+// Views: Variables, breakpoints, registers, expressions, disassembly, SFRs, memory, memory browser
+// Memory and instructions as numbers
+
+int main(void)
+{
+    while (1) {
+        register int counter = 0;
+        counter++;
+        counter++;
+        counter++;
+        counter++;
+        counter++;
+        counter++;
+        counter++;
+        counter--;
+        counter++;
+        counter++;
+        counter++;
+        counter++;
+        counter++;
+        counter++;
+
+        int use_counter = counter;
+    }
+}
